95473ad52b9b5c9f6863b24c0e45af138f69f4cf
Sven Bledt <sven.bledt@gmail.com>
2022-12-06 13:41:30 +0100

    CodeStyle

--- js/countdown.js
+++ js/countdown.js
@@ -189,6 +189,6 @@ var x = setInterval(function() {
 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("stunden").innerHTML = "EXPIRED";
+    document.getElementById("stunden").innerHTML = "HOPE YOU HAD A MARRY CHRISTMAS";
   }
 }, 1000);
